f9e7174e2798cffaaa60b02fb3da56fffba7cba2
tonykc <tonykc@hotmail.com>
2014-08-19 22:01:10 +0800

    Initial version.  Need to create the final tidy data set.

---
+++ run_analysis.R
@@ -0,0 +1,160 @@
+# setwd ("d:\\coursera\\getdata\\UCI HAR Dataset")
+
+read_files <- function (set_of_data) {
+  file_path <- paste (".\\", set_of_data, "\\", sep = "") 
+  file_subject <- paste (file_path, "subject_", set_of_data, ".txt", sep = "")
+  file_data_all <- paste (file_path, "X_", set_of_data, ".txt", sep = "")
+  file_label <- paste (file_path, "y_", set_of_data, ".txt", sep = "")
+  
+  subjects <- read.csv (file_subject, header=FALSE)
+  variables_all <- read.csv (file_data_all, header=FALSE)
+  labels <- read.csv (file_label, header=FALSE)
+  merged_data <- 
+    cbind (subjects, labels, 
+           substr (variables_all$V1, 1, 16), # 1 tBodyAcc-mean()-X
+           substr (variables_all$V1, 17, 32), # 2 tBodyAcc-mean()-Y
+           substr (variables_all$V1, 33, 48), # 3 tBodyAcc-mean()-Z
+           substr (variables_all$V1, 49, 64), # 4 tBodyAcc-std()-X
+           substr (variables_all$V1, 65, 80), # 5 tBodyAcc-std()-Y
+           substr (variables_all$V1, 81, 96), # 6 tBodyAcc-std()-Z
+           substr (variables_all$V1, 641, 656), # 41 tGravityAcc-mean()-X
+           substr (variables_all$V1, 657, 672), # 42 tGravityAcc-mean()-Y
+           substr (variables_all$V1, 673, 688), # 43 tGravityAcc-mean()-Z
+           substr (variables_all$V1, 689, 704), # 44 tGravityAcc-std()-X
+           substr (variables_all$V1, 705, 720), # 45 tGravityAcc-std()-Y
+           substr (variables_all$V1, 721, 736), # 46 tGravityAcc-std()-Z
+           substr (variables_all$V1, 1281, 1296), # 81 tBodyAccJerk-mean()-X
+           substr (variables_all$V1, 1297, 1312), # 82 tBodyAccJerk-mean()-Y
+           substr (variables_all$V1, 1313, 1328), # 83 tBodyAccJerk-mean()-Z
+           substr (variables_all$V1, 1329, 1344), # 84 tBodyAccJerk-std()-X
+           substr (variables_all$V1, 1345, 1360), # 85 tBodyAccJerk-std()-Y
+           substr (variables_all$V1, 1361, 1376), # 86 tBodyAccJerk-std()-Z
+           substr (variables_all$V1, 1921, 1936), # 121 tBodyGyro-mean()-X
+           substr (variables_all$V1, 1937, 1952), # 122 tBodyGyro-mean()-Y
+           substr (variables_all$V1, 1953, 1968), # 123 tBodyGyro-mean()-Z
+           substr (variables_all$V1, 1969, 1984), # 124 tBodyGyro-std()-X
+           substr (variables_all$V1, 1985, 2000), # 125 tBodyGyro-std()-Y
+           substr (variables_all$V1, 2001, 2016), # 126 tBodyGyro-std()-Z
+           substr (variables_all$V1, 2561, 2576), # 161 tBodyGyroJerk-mean()-X
+           substr (variables_all$V1, 2577, 2592), # 162 tBodyGyroJerk-mean()-Y
+           substr (variables_all$V1, 2593, 2608), # 163 tBodyGyroJerk-mean()-Z
+           substr (variables_all$V1, 2609, 2624), # 164 tBodyGyroJerk-std()-X
+           substr (variables_all$V1, 2625, 2640), # 165 tBodyGyroJerk-std()-Y
+           substr (variables_all$V1, 2641, 2656), # 166 tBodyGyroJerk-std()-Z
+           substr (variables_all$V1, 3201, 3216), # 201 tBodyAccMag-mean()
+           substr (variables_all$V1, 3217, 3232), # 202 tBodyAccMag-std()
+           substr (variables_all$V1, 3409, 3424), # 214 tGravityAccMag-mean()
+           substr (variables_all$V1, 3425, 3440), # 215 tGravityAccMag-std()
+           substr (variables_all$V1, 3617, 3632), # 227 tBodyAccJerkMag-mean()
+           substr (variables_all$V1, 3633, 3648), # 228 tBodyAccJerkMag-std()
+           substr (variables_all$V1, 3825, 3840), # 240 tBodyGyroMag-mean()
+           substr (variables_all$V1, 3841, 3856), # 241 tBodyGyroMag-std()
+           substr (variables_all$V1, 4033, 4048), # 253 tBodyGyroJerkMag-mean()
+           substr (variables_all$V1, 4049, 4064), # 254 tBodyGyroJerkMag-std()
+           substr (variables_all$V1, 4241, 4256), # 266 fBodyAcc-mean()-X
+           substr (variables_all$V1, 4257, 4272), # 267 fBodyAcc-mean()-Y
+           substr (variables_all$V1, 4273, 4288), # 268 fBodyAcc-mean()-Z
+           substr (variables_all$V1, 4289, 4304), # 269 fBodyAcc-std()-X
+           substr (variables_all$V1, 4305, 4320), # 270 fBodyAcc-std()-Y
+           substr (variables_all$V1, 4321, 4336), # 271 fBodyAcc-std()-Z
+           substr (variables_all$V1, 5505, 5520), # 345 fBodyAccJerk-mean()-X
+           substr (variables_all$V1, 5521, 5536), # 346 fBodyAccJerk-mean()-Y
+           substr (variables_all$V1, 5537, 5552), # 347 fBodyAccJerk-mean()-Z
+           substr (variables_all$V1, 5553, 5568), # 348 fBodyAccJerk-std()-X
+           substr (variables_all$V1, 5569, 5584), # 349 fBodyAccJerk-std()-Y
+           substr (variables_all$V1, 5585, 5600), # 350 fBodyAccJerk-std()-Z
+           substr (variables_all$V1, 6769, 6784), # 424 fBodyGyro-mean()-X
+           substr (variables_all$V1, 6785, 6800), # 425 fBodyGyro-mean()-Y
+           substr (variables_all$V1, 6801, 6816), # 426 fBodyGyro-mean()-Z
+           substr (variables_all$V1, 6817, 6832), # 427 fBodyGyro-std()-X
+           substr (variables_all$V1, 6833, 6848), # 428 fBodyGyro-std()-Y
+           substr (variables_all$V1, 6849, 6864), # 429 fBodyGyro-std()-Z
+           substr (variables_all$V1, 8033, 8048), # 503 fBodyAccMag-mean()
+           substr (variables_all$V1, 8049, 8064), # 504 fBodyAccMag-std()
+           substr (variables_all$V1, 8241, 8256), # 516 fBodyBodyAccJerkMag-mean()
+           substr (variables_all$V1, 8257, 8272), # 517 fBodyBodyAccJerkMag-std()
+           substr (variables_all$V1, 8449, 8464), # 529 fBodyBodyGyroMag-mean()
+           substr (variables_all$V1, 8465, 8480), # 530 fBodyBodyGyroMag-std()
+           substr (variables_all$V1, 8657, 8672), # 542 fBodyBodyGyroJerkMag-mean()
+           substr (variables_all$V1, 8673, 8688) # 543 fBodyBodyGyroJerkMag-std()
+    )
+  colnames (merged_data) <- 
+    c ('subject', 'activity_code',
+       'tBodyAcc-mean()-X',
+       'tBodyAcc-mean()-Y',
+       'tBodyAcc-mean()-Z',
+       'tBodyAcc-std()-X',
+       'tBodyAcc-std()-Y',
+       'tBodyAcc-std()-Z',
+       'tGravityAcc-mean()-X',
+       'tGravityAcc-mean()-Y',
+       'tGravityAcc-mean()-Z',
+       'tGravityAcc-std()-X',
+       'tGravityAcc-std()-Y',
+       'tGravityAcc-std()-Z',
+       'tBodyAccJerk-mean()-X',
+       'tBodyAccJerk-mean()-Y',
+       'tBodyAccJerk-mean()-Z',
+       'tBodyAccJerk-std()-X',
+       'tBodyAccJerk-std()-Y',
+       'tBodyAccJerk-std()-Z',
+       'tBodyGyro-mean()-X',
+       'tBodyGyro-mean()-Y',
+       'tBodyGyro-mean()-Z',
+       'tBodyGyro-std()-X',
+       'tBodyGyro-std()-Y',
+       'tBodyGyro-std()-Z',
+       'tBodyGyroJerk-mean()-X',
+       'tBodyGyroJerk-mean()-Y',
+       'tBodyGyroJerk-mean()-Z',
+       'tBodyGyroJerk-std()-X',
+       'tBodyGyroJerk-std()-Y',
+       'tBodyGyroJerk-std()-Z',
+       'tBodyAccMag-mean()',
+       'tBodyAccMag-std()',
+       'tGravityAccMag-mean()',
+       'tGravityAccMag-std()',
+       'tBodyAccJerkMag-mean()',
+       'tBodyAccJerkMag-std()',
+       'tBodyGyroMag-mean()',
+       'tBodyGyroMag-std()',
+       'tBodyGyroJerkMag-mean()',
+       'tBodyGyroJerkMag-std()',
+       'fBodyAcc-mean()-X',
+       'fBodyAcc-mean()-Y',
+       'fBodyAcc-mean()-Z',
+       'fBodyAcc-std()-X',
+       'fBodyAcc-std()-Y',
+       'fBodyAcc-std()-Z',
+       'fBodyAccJerk-mean()-X',
+       'fBodyAccJerk-mean()-Y',
+       'fBodyAccJerk-mean()-Z',
+       'fBodyAccJerk-std()-X',
+       'fBodyAccJerk-std()-Y',
+       'fBodyAccJerk-std()-Z',
+       'fBodyGyro-mean()-X',
+       'fBodyGyro-mean()-Y',
+       'fBodyGyro-mean()-Z',
+       'fBodyGyro-std()-X',
+       'fBodyGyro-std()-Y',
+       'fBodyGyro-std()-Z',
+       'fBodyAccMag-mean()',
+       'fBodyAccMag-std()',
+       'fBodyBodyAccJerkMag-mean()',
+       'fBodyBodyAccJerkMag-std()',
+       'fBodyBodyGyroMag-mean()',
+       'fBodyBodyGyroMag-std()',
+       'fBodyBodyGyroJerkMag-mean()',
+       'fBodyBodyGyroJerkMag-std()'
+       )
+  return (merged_data)
+}
+
+test_data <- read_files ("test")
+train_data <- read_files ("train")
+
+combined_data <- rbind (test_data, train_data)
+activity_label <- read.fwf ("activity_labels.txt", c(2, 18))
+colnames (activity_label) <- c ("activity_code", "activity")
+combined_data <- merge (combined_data, activity_label, 
+                        by.x = "activity_code", by.y = "activity_code")
